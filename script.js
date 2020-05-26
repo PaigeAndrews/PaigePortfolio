596b033163030049751d70780664a469ef197381
Paige Andrews <pandrews25@yahoo.com>
2020-05-25 23:37:26 -0400

    studied for for 10 hours, adding new project detail section

--- script.js
+++ script.js
@@ -32,7 +32,59 @@ pigButton = document.getElementById("pigButton");
 gamingButton = document.getElementById("gamingButton");
 animalButton = document.getElementById("animalButton");
 hangmanButton = document.getElementById("hangmanButton");
+projectDetailContainer = document.getElementById("projectDetail");
+projectDetailTitle = document.getElementById("projectDetailTitle");
+projectDetailImage = document.getElementById("projectDetailImage");
 
-btn1.addEventListener("click", function(){
+// btn1.addEventListener("click", function(){
   
-})
+// })
+
+arrIds = [
+  pigButton,
+  gamingButton,
+  animalButton,
+  hangmanButton
+]
+
+projectTitle = [
+  "Dice Pige Game",
+  "React Game Search",
+  "Animal Compatibility",
+  "Halloween Hangman"
+]
+projectImage =[]
+
+
+let number = 0;
+
+for (let h=0; h<4; h++){
+  arrIds[h].addEventListener("click", function(){
+
+    if (number > 0){
+      projectDetailTitle.removeChild(projectDetailTitle.childNodes[0]);
+      // animalParagraph.removeChild(animalParagraph.childNodes[0]);
+      // animalParagraphRightImage.removeChild(animalParagraphRightImage.childNodes[0]);
+    }
+    
+    // animalParagraph.style.display = "inline-block";
+    // animalParagraphRightImage.style.display = "inline-block";
+
+    let textnodeIds = document.createTextNode(arrIds[h])
+    let textnodeTitle = document.createTextNode(projectTitle[h]);
+    // let textnodeDes = document.createTextNode(arrDescription[h]);
+    // let textnodeImage = document.createTextNode(descriptionImages[h]);
+
+    // animalParagraphRightImage.src = descriptionImages[h];
+
+
+    // arrIds[h].appendChild(textnodeIds);
+    projectDetailTitle.appendChild(textnodeTitle);
+    // animalParagraph.appendChild(textnodeDes);
+    // animalParagraphRightImage.appendChild(textnodeImage);
+
+    projectDetailTitle.scrollIntoView()
+    number++
+
+  })
+} 
